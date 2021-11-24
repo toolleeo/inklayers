c7de1a082264acf9eec517f884f51b4819703091
Tullio Facchinetti <tullio.facchinetti@unipv.it>
2021-11-24 17:13:00 +0100

    DEL: unsed, old scripts.

--- export-command.sh
+++
@@ -1,3 +0,0 @@
-python svg-export-layers.py -c bug2-closer.json bug2-closer.svg
-# ./svg-objects-export.py --xpath "//svg:g[@inkscape:groupmode='layer']" --extra '--export-area-page --export-id-only' my-image.svg
-gs -q -dNOPAUSE -dBATCH -sDEVICE=pdfwrite -sOutputFile=out.pdf bug2-closer-?.pdf
